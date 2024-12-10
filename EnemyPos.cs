a5f912fe6ae7c007bac9fbe93e7a03af05308398
NoteBoxz <139518687+NoteBoxz@users.noreply.github.com>
2024-12-09 23:10:44 -0500

    whoops again

--- EnemyPos.cs
+++ EnemyPos.cs
@@ -18,16 +18,17 @@ namespace LCMoniterEnemies
 
         public void LateUpdate()
         {
-            if (Root.isEnemyDead && LCMoniterEnemies.AutoSwitchOnEnemyDeath.Value)
-            {
-                Destroy(gameObject);
-            }
             // Set the object's position relative to the parent
             transform.localPosition = new Vector3(
                 transform.localPosition.x, // Keep the current local X position
                 LCMoniterEnemies.TargetYoffset.Value,               // Set Y to the desired offset
                 transform.localPosition.z  // Keep the current local Z position
             );
+            
+            if (Root.isEnemyDead && LCMoniterEnemies.AutoSwitchOnEnemyDeath.Value)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
